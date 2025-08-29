f734965e257aa4a83ffda70061d05130b24757c0
Cody McCarty <codyjmccarty@gmail.com>
2025-08-29 13:30:12 -0500

    c1.l11 custom handlers

--- main.go
+++ main.go
@@ -10,7 +10,19 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	fileServer := http.FileServer(http.Dir(filepathRoot))
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain")
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	})
 
 	srv := &http.Server{
 		Addr:    ":" + port,
