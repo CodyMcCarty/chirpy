31834037c1bf0266c4a70e4c392319722e861b16
Cody McCarty <codyjmccarty@gmail.com>
2025-08-29 23:35:09 -0500

    c5.l5 create user

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -15,6 +17,7 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
+	platform       string
 }
 
 func main() {
@@ -33,9 +36,12 @@ func main() {
 	}
 	dbQueries := database.New(dbConn)
 
+	pf := os.Getenv("PLATFORM")
+
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             dbQueries,
+		platform:       pf,
 	}
 
 	mux := http.NewServeMux()
@@ -48,6 +54,8 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
+	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -55,4 +63,33 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func (apiConfig *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var params parameters
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	usr, err := apiConfig.db.CreateUser(context.Background(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(usr)
+	if err != nil {
+		log.Printf("Error marshalling user: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(dat)
 }
--- reset.go
+++ reset.go
@@ -1,8 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+)
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	err := cfg.db.DeleteUsers(context.Background())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
