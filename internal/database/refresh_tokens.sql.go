29f998c40c6a9014b5d3bd25ad4f44c8a2c3c26a
Cody McCarty <codyjmccarty@gmail.com>
2025-08-31 11:05:35 -0500

    c7.l1 auth

--- internal/database/refresh_tokens.sql.go
+++ internal/database/refresh_tokens.sql.go
@@ -46,7 +46,7 @@ func (q *Queries) CreateRefreshToken(ctx context.Context, arg CreateRefreshToken
 
 const getUserFromRefreshToken = `-- name: GetUserFromRefreshToken :one
 SELECT users.id, users.created_at, users.updated_at, users.email, users.hashed_password FROM users
-                        JOIN refresh_tokens ON users.id = refresh_tokens.user_id
+JOIN refresh_tokens ON users.id = refresh_tokens.user_id
 WHERE refresh_tokens.token = $1
   AND revoked_at IS NULL
   AND expires_at > NOW()
--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
 
+	"github.com/CodyMcCarty/chirpy/internal/auth"
 	"github.com/CodyMcCarty/chirpy/internal/database"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -61,6 +67,7 @@ func main() {
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
@@ -76,4 +83,93 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerUsersUpdate(w http.ResponseWriter, r *http.Request) {
+	type parameter struct {
+		Password string `json:"password"`
+		Email    string `json:"email"`
+	}
+	type response struct {
+		User
+	}
+
+	// users can update their own (but not others') email and password
+	// If the access token is malformed or missing, respond with a 401 status code.
+
+	refreshToken, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, 401, "Couldn't find token", err)
+		return
+	}
+
+	userId, err := UserIDFromJWT(refreshToken, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, 401, "Couldn't find user", err)
+	}
+
+	params := parameter{}
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	// Hash the password,
+	hashedPassword, err := auth.HashPassword(params.Password)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't hash password", err)
+		return
+	}
+
+	// then update the hashed password and the email for the authenticated user in the database.
+	userDbUp, err := cfg.db.UpdateUser(r.Context(), database.UpdateUserParams{
+		ID:             userId,
+		Email:          params.Email,
+		HashedPassword: hashedPassword,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update user", err)
+		return
+	}
+
+	// Respond with a 200 if everything is successful and the newly updated User resource (omitting the password of course).
+	respondWithJSON(w, http.StatusOK, response{
+		User{
+			ID:        userDbUp.ID,
+			CreatedAt: userDbUp.CreatedAt,
+			UpdatedAt: userDbUp.UpdatedAt,
+			Email:     userDbUp.Email,
+		},
+	})
+
+}
+
+func UserIDFromJWT(tokenStr, secret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenStr,
+		&jwt.RegisteredClaims{},
+		func(t *jwt.Token) (interface{}, error) {
+			if t.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+				return nil, fmt.Errorf("unexpected signing method: %s", t.Method.Alg())
+			}
+			return []byte(secret), nil
+		},
+		jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}),
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	rc, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok || !token.Valid {
+		return uuid.Nil, errors.New("invalid token")
+	}
+
+	// Optional, but your tokens show iss:"chirpy-access"
+	if rc.Issuer != "chirpy-access" {
+		return uuid.Nil, fmt.Errorf("invalid issuer: %s", rc.Issuer)
+	}
+
+	return uuid.Parse(rc.Subject)
 }
